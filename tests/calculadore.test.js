b6e69d83c5dc76e3981a3909bed3f830d3f80c5d
Jairo Gonçalves <152689394+JairoDevBR@users.noreply.github.com>
2024-11-27 10:48:15 +0000

    teste de teste calculadora

--- tests/calculadore.test.js
+++ tests/calculadore.test.js
@@ -1,3 +1,11 @@
-test("espero que 1 seja 1", () => {
-  expect(1).toBe(1);
+const calculadora = require("../models/calculadora");
+
+test("somar 5 + 100 deveria retornar 4", () => {
+  const resultado = calculadora.somar(5, 100);
+  expect(resultado).toBe(105);
+});
+
+test("somar string + 100 deveria retornar Error", () => {
+  const resultado = calculadora.somar("string", 100);
+  expect(resultado).toBe("Error");
 });

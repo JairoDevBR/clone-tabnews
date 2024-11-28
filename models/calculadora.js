11580e61d9533fe8838200acc14282ab96a9c3ef
Jairo Gonçalves <152689394+JairoDevBR@users.noreply.github.com>
2024-11-28 11:06:26 +0000

    teste de status

--- models/calculadora.js
+++
@@ -1,9 +0,0 @@
-function somar(numero1, numero2) {
-  if (typeof numero1 === "string" || numero2 === "string") {
-    return "Error";
-  } else {
-    return numero1 + numero2;
-  }
-}
-
-exports.somar = somar;

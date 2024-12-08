819e75e30a59a6ac5cfdd20885938199db5c6a26
Jairo Gonçalves <152689394+JairoDevBR@users.noreply.github.com>
2024-12-08 09:42:51 +0000

    version aplicada ao teste

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -3,8 +3,16 @@ import database from "infra/database.js";
 async function status(request, response) {
   const uptadeAt = new Date().toISOString();
 
+  const databaseVersionREsult = await database.query("SHOW server_version;");
+  const databaseVersionVlue = databaseVersionREsult.rows[0].server_version;
+
   response.status(200).json({
     updated_at: uptadeAt,
+    dependencies: {
+      database: {
+        version: databaseVersionVlue,
+      },
+    },
   });
 }
 
